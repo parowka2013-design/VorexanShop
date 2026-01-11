218b7a7fe2e904fc91774cb3683c3e966f83dd2b
parowka2013-design <parowka2013@gmail.com>
2026-01-11 16:32:38 +0100

    Delete functions/api/user directory

--- functions/api/user/[id].js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const { params } = context;
-  const userId = params.id;
-  
-  return new Response(
-    JSON.stringify({ userId, message: `User ${userId} profile` }),
-    { headers: { 'Content-Type': 'application/json' } }
-  );
-}
